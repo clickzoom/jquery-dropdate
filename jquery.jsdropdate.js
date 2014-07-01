8b22fb49c6eedfc61299c93a3fa48c18c8203528
lhc772 <lhc772@yahoo.com>
2014-07-01 18:09:01 +0700

    init

---
+++ jquery.jsdropdate.js
@@ -0,0 +1,10 @@
+(function( $ ) {
+	var default_options = {
+		months: [
+				"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+		],
+		dayOfWeek: [
+				"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+		]
+	};
+}
